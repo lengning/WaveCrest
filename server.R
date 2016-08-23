47a677d894f1806211de1afee988acfdc98233e8
Rhonda Bacher <rhonda.bacher@gmail.com>
2016-08-23 15:00:40 -0500

    fix output folder problem

--- server.R
+++ server.R
@@ -15,7 +15,8 @@ shinyServer(function(input, output, session) {
   
   In <- reactive({
     print(input$Outdir)
-    outdir <- paste0("~/",input$Outdir[[1]][[2]],"/")
+	outdir <- paste0("~", do.call("file.path", input$Outdir[[1]]), "/")
+    # outdir <- paste0("~/",input$Outdir[[1]][[2]],"/")
     print(outdir)
     
     the.file <- input$filename$name
